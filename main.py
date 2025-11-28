3eb76a20b245d9d4f727bf0d8029b0499f82bd3b
DvrkGh0sty <mutsvairoethan@gmail.com>
2025-11-28 16:04:24 +0300

    'blank'

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+import random
+
+General_knowledge = {[
+    'What country has the largest population in the world?': 'India',
+    'What is the largest planet in our solar system': 'Jupiter'.,
+    'Who wrote the play romeo and Juliet': 'Shakespear'
+]
+}
+Math = {
+    'What is the derivative of 3x² + 5x': '6x+5',
+    'If a triangle has sides 6, 8, and 10, what type of triangle is it?': 'right angled triangle',
+    'Convert 0.375 into a fraction': '37.5/100'
+
+}
+
+Programming = {
+    'What does OOP stand for in programming': 'Object Oriented Programming',
+    'In python, what keyword is used to define a function': 'def',
+    'What data structure uses variables to vars': 'lists' 
+}
+print('Welcome to quiz game')
+decision = input('What would you like to be tested on: math,programming and General_knowledge')
+if decision in ['math', 'maths', 'mathematics']:
+    for key in general_knowledge['']
